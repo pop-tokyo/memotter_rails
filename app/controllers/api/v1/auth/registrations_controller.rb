345a87765de121f962d06b384abf4b4e2e13feeb
popmac <parade8823@gmail.com>
2019-08-18 15:25:35 +0900

    wrap_parameters falseを設定
    アカウント作成やログイン時に Unpermitted parameter: :registration が出ないようにするため

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  wrap_parameters false
+
   private
   def sign_up_params
     params.permit(:username, :email, :password, :password_confirmation)
